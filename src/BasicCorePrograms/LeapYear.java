6956a68c584a3698edd2ab0f4e17f074a6b7ecf7
akashbg3010 <akashbpura@gmail.com>
2021-12-15 01:51:10 +0530

    Added LaepYear.java file

---
+++ src/BasicCorePrograms/LeapYear.java
@@ -0,0 +1,38 @@
+package BasicCorePrograms;
+
+public class LeapYear {
+
+public static void main(String[] args) {
+		
+		int leapYear = 2000;
+		boolean isLeapYear = false;
+		
+		if(String.valueOf(leapYear).length() == 4) {
+			if( leapYear % 4 == 0) {
+				if(leapYear % 100 == 0) {
+					if(leapYear % 400 == 0) {
+						isLeapYear = true;
+					}
+					else {
+						isLeapYear = false;
+					}
+				}
+				else {
+					isLeapYear = true;
+				}
+			}
+			else {
+				isLeapYear = false;
+			}
+		}
+		else {
+			System.out.println("Year should be a 4 digit number");
+		}
+		if(isLeapYear == true) {
+			System.out.println(leapYear + "is a leap year");
+		}
+		else {
+			System.out.println(leapYear + "is not leap year");
+		}
+	}
+}
